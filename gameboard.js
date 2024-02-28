bbe6cb8c55807487b08b26f35831eff0d0ae6200
Ryan Wong <ryanjameswong@outlook.com>
2024-02-28 10:25:32 -0500

    create functions to check if coordinate is valid and free before placing ship, test isValidCoordinate function

--- gameboard.js
+++ gameboard.js
@@ -6,24 +6,50 @@ let Gameboard = function (dimension) {
     .fill(null)
     .map(() => new Array(dimension).fill(null));
 
-  gameboard.placeShip = function (
-    x,
-    y,
-    length,
-    orientation
-  ) {
+  gameboard.placeShip = function (x, y, length, orientation) {
+    if (!isValidCoordinate(x, y, length, orientation)) {
+      return false;
+    }
+
+    if (!coordinatesFree(x, y, length, orientation)) {
+      return false;
+    }
     const newShip = Ship(length);
 
     if (orientation === "horizontal") {
-      for (let i = x; i < length; i++) {
+      for (let i = x; i < x + length; i++) {
         gameboard.board[y][i] = newShip;
       }
     } else {
-      for (let i = y; i < length; i++) {
+      for (let i = y; i < y + length; i++) {
         gameboard.board[i][x] = newShip;
       }
     }
   };
+
+  function coordinatesFree(x, y, length, orientation) {
+    if (orientation === "horizontal") {
+      for (let i = x; i < x + length; i++) {
+        if (gameboard.board[y][i] !== null) {
+          return false;
+        }
+      }
+    } else {
+      for (let i = y; i < y + length; i++) {
+        if (gameboard.board[i][x] !== null) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  function isValidCoordinate(x, y, length, orientation) {
+    if (orientation === "horizontal") {
+      return x >= 0 && x + length < gameboard.board[0].length && length > 0;
+    } else {
+      return y >= 0 && y + length < gameboard.board.length && length > 0;
+    }
+  }
 
   return gameboard;
 };
--- gameboard.test.js
+++ gameboard.test.js
@@ -12,10 +12,45 @@ describe("Gameboard", () => {
     ]);
   });
 
-  test("place ship on gameboard", () => {
-    const gameboard = Gameboard(2);
-    gameboard.placeShip(0, 0, 2, 'horizontal');
+  test("place ships on gameboard vertically and horizontally", () => {
+    const gameboard = Gameboard(4);
+
+    //Should not be null at horizontally placed coordinates
+    gameboard.placeShip(0, 0, 2, "horizontal");
     expect(gameboard.board[0][0]).toBeDefined();
     expect(gameboard.board[0][1]).toBeDefined();
+
+    //Should have objects at gameboard.board[1][1] and gameboard.board[2][1]
+    gameboard.placeShip(1, 1, 2, "vertical");
+    expect(gameboard.board[1][1]).toEqual(
+      expect.objectContaining({
+        length: 2,
+        hitsNumber: 0,
+        isSunk: expect.any(Function),
+        hit: expect.any(Function),
+      })
+    );
+    expect(gameboard.board[2][1]).toEqual(
+      expect.objectContaining({
+        length: 2,
+        hitsNumber: 0,
+        isSunk: expect.any(Function),
+        hit: expect.any(Function),
+      })
+    );
+  });
+
+  test("do not allow ship placement if invalid coordinate or length", () => {
+    const gameboard = Gameboard(4);
+
+    //Invalid x
+    expect(gameboard.placeShip(-1, 0, 1, "horizontal")).toEqual(false);
+    //No length
+    expect(gameboard.placeShip(2, 0, 0, "vertical")).toEqual(false);
+    //Invalid y
+    expect(gameboard.placeShip(2, 4, 1, "vertical")).toEqual(false);
+    //Out of bounds
+    expect(gameboard.placeShip(2, 2, 4, "horizontal")).toEqual(false);
+
   });
 });
